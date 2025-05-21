67d91b32845fcee9d82b28d0e29b630e034d875e
USUARIO <perebercerotrigell@gmail.com>
2025-05-21 23:01:45 +0200

    El primer commit del ejercicios1.py

---
+++ ejercicios1.py
@@ -0,0 +1,32 @@
+#Ejercicios Hola Mundo
+#Ejercicio 1
+#1. Imprime "¡Hola Mundo!" por consola.
+print("¡Hola Mundo!")
+print("Mi nombres es Pere y tengo 31 años")
+
+#2. Escribe un comentario de una sola línea explicando qué
+# hace el siguiente código del ejercicio 1
+"""
+El código de el primer ejercicio imprime por consola el texto "¡Hola Mundo!" y el segundo imprime mi nombre y edad.
+
+
+"""
+#3. Imprime tu nombre y edad en la misma línea utilizando la
+#función print.
+
+"""4. Usa la función type() para imprimir el tipo de dato de
+una cadena de texto, un número entero y un número
+decimal."""
+
+print(type("Hola"))
+print(type(3))
+print(type(3.0))
+
+"""
+5. Escribe un comentario en varias líneas explicando qué
+son los tipos de datos en Python.
+"""
+"""
+Los tipos de datos en Python son una forma de clasificar los diferentes tipos de valores que pueden ser almacenados y manipulados en un programa. Python tiene varios tipos de datos incorporados, incluyendo enteros (int), números de punto flotante (float), cadenas de texto (str), listas (list), tuplas (tuple), conjuntos (set) y diccionarios (dict). Cada tipo de dato tiene sus propias características y métodos asociados, lo que permite a los programadores elegir el tipo más adecuado para sus necesidades específicas. Además, Python es un lenguaje dinámicamente tipado, lo que significa que no es necesario declarar el tipo de dato de una variable al momento de su creación.
+"""
+
