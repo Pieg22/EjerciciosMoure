e89be112e6f73e25d8db4f44cb26d737e6e2582a
USUARIO <perebercerotrigell@gmail.com>
2025-05-24 22:15:09 +0200

    Update 02_variables.py

--- 02_variables.py
+++ 02_variables.py
@@ -1,2 +1,31 @@
 #En este apartado empezamos con las variables ofc . 
 
+"""1. Declara y asigna valores a las siguientes variables:
+name: una cadena que contenga tu nombre.
+age: un número entero que represente tu edad.
+height: un número flotante que represente tu altura.
+Imprime cada variable en una línea separada.
+"""
+
+name = "Pere"
+age = 31
+height = 1,80
+
+print(name)
+print(age)
+print(height)
+
+"""2. Convierte la variable edad de entero a cadena y
+concatenala con un texto que diga cuántos años tienes.
+
+"""
+age = "31"
+print(" Tengo " + age + " años ")
+
+"""3. Declara una variable booleana is_student que indique si
+eres estudiante o no. Usa True o False según corresponda
+e imprímela.
+"""
+
+
+
