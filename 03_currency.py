9f56630e44348d32f6782a885874ae17d2ca76c4
USUARIO <perebercerotrigell@gmail.com>
2025-05-30 22:32:52 +0200

    Create 03_currency.py
    ask the user for the amount they have in pesos , soles and reais and calculates the total
    in USD .
    
    Make sure to google the current exchange rates!

---
+++ 03_currency.py
@@ -0,0 +1,27 @@
+"""Info 
+1 CO (Colombian Peso)= 0,00024 USD
+1 PE (Peruvian Soles)= 0,28 USD
+1 RB ( Brazilian reais)= 0,17 USD 
+"""
+
+#Ask to the user 
+
+CO = input(" How much CO do you have ? ")
+PE = input(" How PE do you left ?  ")
+RB = input(" Do you have some RB? tell me the amount ")
+
+# Intercambio de moneda COM (Colombian Peso Money)
+
+COM =( CO*0.00024)  
+PEM =( 0,28 * PE )
+RBM =( 0,17 * RB) 
+
+#Sum results and print in USD 
+
+print(COM + PEM + RBM)
+
+
+
+
+
+
