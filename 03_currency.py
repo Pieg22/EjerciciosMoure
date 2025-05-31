a4d112e0cb200084add8a848c4fffa2de791c880
USUARIO <perebercerotrigell@gmail.com>
2025-05-31 22:06:00 +0200

    Update 03_currency.py
    Now work on!

--- 03_currency.py
+++ 03_currency.py
@@ -6,19 +6,31 @@
 
 #Ask to the user 
 
-CO = input(" How much CO do you have ? ")
-PE = input(" How PE do you left ?  ")
-RB = input(" Do you have some RB? tell me the amount ")
+CO_str = input(" How much CO do you have ? ")
+PE_str = input(" How PE do you left ?  ")
+RB_str = input(" Do you have some RB? tell me the amount ")
 
-# Intercambio de moneda COM (Colombian Peso Money)
+#Transform the str to floats , because we need to calculate It later 
 
-COM =( CO*0.00024)  
-PEM =( 0,28 * PE )
-RBM =( 0,17 * RB) 
+CO = float(CO_str)
+PE = float(PE_str)
+RB = float(RB_str)
+
+# Intercambio de moneda CO_M (Colombian Peso Money)
+
+CO_M = 0.00024
+PE_M = 0.28 
+RB_M = 0.17  
+
+#Calculates 
+
+result_COM = CO_M * CO 
+result_PEM = PE_M * PE
+result_RBM = RB_M * RB
 
 #Sum results and print in USD 
 
-print(COM + PEM + RBM)
+print("Do you have : ", result_COM + result_PEM + result_RBM ,"USD")
 
 
 
