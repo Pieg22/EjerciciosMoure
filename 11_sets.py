77ac28edb97a38edad14155b2df2d092483505d9
Pere <perebercerotrigell@gmail.com>
2025-06-21 09:01:53 +0200

    Starting with sets.
    :)

---
+++ 11_sets.py
@@ -0,0 +1,22 @@
+# 1. Crea un set con los nÃºmeros del 1 al 5 e imprÃ­melo. 
+
+my_fisrt_set = {1, 2, 3, 4, 5}
+
+print(my_fisrt_set)
+print(type(my_fisrt_set))
+
+
+# 2. AÃ±ade el nÃºmero 6 al set {1, 2, 3, 4, 5} e imprÃ­melo.
+
+my_fisrt_set.add(6)
+
+print(my_fisrt_set)
+
+# 3. Intenta aÃ±adir el nÃºmero 5 al set {1, 2, 3, 4, 5} nuevamente. Â¿QuÃ© sucede? 
+
+my_fisrt_set.add(5) #It do not nothing , even no add but also It do not report a error. Just stay..
+
+# 4. Verifica si el nÃºmero 3 estÃ¡ en el set {1, 2, 3, 4, 5} e imprime el resultado. 
+
+my_fisrt_set.
+
