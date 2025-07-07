7f160e926ded04aac854b85c55cb34466f6c0af2
Pere <perebercerotrigell@gmail.com>
2025-07-07 07:42:03 +0200

    Almost done

--- 11_fuctions.py
+++ 11_fuctions.py
@@ -2,14 +2,12 @@
 
 # 1. Crea una funciÃ³n llamada "personalized_greeting" que reciba un nombre como argumento e imprima "Hola, <nombre>". Si no se proporciona ningÃºn nombre, debe saludar diciendo "Hola, desconocido".
 
-def personalized_greeting(name):
-    if name :
-        print(f"Hola {name}")
-    
-    elif not name :
-        print("Hola , desconocido") 
+def personalized_greeting(name="unkown"):
+    print(f"Hola , {name}")
+
+   
         
-personalized_greeting("Pere")
+
 
 # 2. Escribe una funciÃ³n llamada "multiply" que reciba dos nÃºmeros como argumentos y retorne el resultado de multiplicarlos.
 
@@ -20,4 +18,52 @@ def multiply(number_one, number_two) :
 multiply(5, 18)
 
 # 3. Crea una funciÃ³n llamada "is_even" que reciba un nÃºmero entero como argumento y retorne True si es par y False si es impar.
+
+def is_even(number): 
+   return number % 2 == 0
+
+# 4. Escribe una funciÃ³n llamada "convert_to_uppercase" que reciba una cadena de texto y la retorne en mayÃºsculas.
+def convert_to_uppercase(str):
+    return str.upper()
+
+# 5. Crea una funciÃ³n llamada "arbitrary_sum" que reciba un nÃºmero arbitrario de nÃºmeros como argumentos y retorne la suma de todos ellos.
+def arbitrary_sum(a,b):
+    sum = a + b
+    return  sum
+# 6. Escribe una funciÃ³n llamada "generate_full_greeting" que reciba dos argumentos: nombre y apellido, y retorne el saludo completo "Hola, <nombre> <apellido>". Los argumentos deben ser pasados por clave.
+
+def generate_full_greeting(name:"Unknown", surname:"Unkwnown"):
+    return (f"Hola , {name} {surname}")
+
+# 7. Crea una funciÃ³n llamada "power" que reciba dos nÃºmeros: base y exponente, y retorne el resultado de elevar la base al exponente.
+
+def power(base, exponent):
+    return base ** exponent 
+
+# 8. Escribe una funciÃ³n llamada "calculate_average" que reciba tres nÃºmeros y retorne su promedio.
+
+def calculate_average(a, b, c):
+    calculate = a+b+c
+    average = calculate/3
+    return average 
+
+
+# 9. Crea una funciÃ³n llamada "count_characters" que reciba una cadena de texto y retorne el nÃºmero de caracteres que contiene.
+
+def count_characters(str):
+    return str.count()
+
+# 10. Escribe una funciÃ³n llamada "display_messages" que reciba un nÃºmero indefinido de cadenas y las imprima en mayÃºsculas, una por una, tal como se hizo en el archivo proporcionado.
+
+def display_messages(str):
+    
+
+
+
+
+
+
+    
+
+
 
