dba7216c1a28b78f8ed9776ff11f4282e87ba515
Pere <perebercerotrigell@gmail.com>
2025-07-10 22:39:57 +0200

    Added 12_class.py

---
+++ 12_class.py
@@ -0,0 +1,26 @@
+#Clases#
+
+# 1. Crea una clase llamada "Animal" que tenga una propiedad "species" y un mÃ©todo "make_sound" que imprima un sonido genÃ©rico.
+class Animal :
+    def __init__(self,species,make_sound): #Constructor + parameters
+        self.species = species
+        self.make_sound = make_sound
+    
+perro = Animal(species="mamifero",make_sound="Guau miau") #Make the object from animal clas
+print(perro.make_sound)
+
+
+
+# 2. Modifica la clase "Animal" para que reciba la especie al crear un objeto y almacÃ©nala en una propiedad pÃºblica. AÃ±ade el mÃ©todo "make_sound" que imprima un sonido dependiendo de la especie.
+
+
+
+        
+        
+
+        
+    
+        
+        
+        
+        
