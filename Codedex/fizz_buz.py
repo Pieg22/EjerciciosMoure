f6a9b448c7d5462342e3d0ad821388432cc164e7
Pere <perebercerotrigell@gmail.com>
2025-08-06 06:57:15 +0200

    fizz_buz.py

--- Codedex/fizz_buz.py
+++ Codedex/fizz_buz.py
@@ -2,4 +2,31 @@
 #For multiples of 3 , print "Fizz" instead of the number 
 #For multiples of 5 , print "Buzz" instead of the number 
 #Here's the tricky part : For multiples of 3 and 5 , print "FizzBuzz"
+element = 0
 
+for element in range(0,101) :    #bucle for will do just the operation ones .
+    while element / 3 == 0 :
+        print("Fizz")
+        
+        
+    while element / 5 == 0 : 
+        print("Buzz")
+        
+        
+    while element / 3 == 0 and element / 5 == 0 :
+        print("FizzBuzz")
+        
+        
+    break 
+
+while element :
+    for element / 3 in ZeroDivisionError :
+        print("N")
+    
+
+                  
+        
+         
+               
+ 
+    
